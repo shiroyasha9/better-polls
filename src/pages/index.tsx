fc348fa6e023b99882550995391dc7466547bcf0
Mubin Ansari <mubinansari910@gmail.com>
2022-08-08 23:15:03 +0530

    add trpc

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,16 @@
 import type { NextPage } from 'next';
-import { prisma } from '../db/client';
+import { trpc } from '../utils/trpc';
 
 const Home: NextPage = (props: any) => {
-  return (
-    <div>
-      Hello world
-      <code>{props.questions}</code>
-    </div>
-  );
+  const { data, isLoading } = trpc.useQuery(['getAllQuestions']);
+
+  if (isLoading || !data) {
+    return <div>Loading...</div>;
+  }
+
+  console.log(data);
+
+  return <div>{data[0]?.question}</div>;
 };
 
 export default Home;
-
-export const getServerSideProps = async () => {
-  const questions = await prisma.pollQuestion.findMany();
-
-  return {
-    props: {
-      questions: JSON.stringify(questions)
-    }
-  };
-};
