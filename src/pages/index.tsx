0aeb6e8f19bea61c3d647dc75fb58d20482a0de5
Mubin Ansari <mubinansari910@gmail.com>
2022-08-08 22:42:36 +0530

    setup prisma

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,23 @@
 import type { NextPage } from 'next';
+import { prisma } from '../db/client';
 
-const Home: NextPage = () => {
-  return <div>Hello world</div>;
+const Home: NextPage = (props: any) => {
+  return (
+    <div>
+      Hello world
+      <code>{props.questions}</code>
+    </div>
+  );
 };
 
 export default Home;
+
+export const getServerSideProps = async () => {
+  const questions = await prisma.pollQuestion.findMany();
+
+  return {
+    props: {
+      questions: JSON.stringify(questions)
+    }
+  };
+};
